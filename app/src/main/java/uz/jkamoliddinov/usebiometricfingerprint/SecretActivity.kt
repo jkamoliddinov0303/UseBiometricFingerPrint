156da956ccdc930bf3da1e9ff4040ead0c35fb03
Jamoliddin <https://github.com/jkamoliddinov0303/MyUztelecomNewFeatures>
2021-08-30 10:17:32 +0500

    Initial commit

---
+++ app/src/main/java/uz/jkamoliddinov/usebiometricfingerprint/SecretActivity.kt
@@ -0,0 +1,11 @@
+package uz.jkamoliddinov.usebiometricfingerprint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecretActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secret)
+    }
+}
